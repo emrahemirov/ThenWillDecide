f5fbbeaf18d0d806d8b56e2c0785c46f44b86ae0
Emrah Emirov <me@emrahemirov.com>
2024-12-07 02:08:06 +0300

    add acceleration and optimize movement code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,22 +4,41 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick joystick;
     [SerializeField] private GameObject planeGameObject;
+
+    [Header("Movement")] 
     [SerializeField] private float verticalMoveSpeed = 15f;
     [SerializeField] private float horizontalMoveSpeed = 10f;
-
+    [SerializeField] private float acceleration = 40f;
+    [SerializeField] private float deceleration = 60f;
     [SerializeField] private float horizontalOffset = 0.5f;
     [SerializeField] private float verticalOffset = 0.5f;
 
     private Vector3 _input;
+    private Vector3 _currentVelocity;
     private Rigidbody _rb;
-    private Collider _planeCollider;
-    private Collider _playerCollider;
+
+    private float _minHorizontalMovable;
+    private float _maxHorizontalMovable;
+    private float _minVerticalMovable;
+    private float _maxVerticalMovable;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
-        _planeCollider = planeGameObject.GetComponent<Collider>();
-        _playerCollider = GetComponent<Collider>(); // Get the player's collider
+        var planeCollider = planeGameObject.GetComponent<Collider>();
+        var playerCollider = GetComponent<Collider>();
+
+        var playerHalfWidth = playerCollider.bounds.extents.x;
+        var playerHalfHeight = playerCollider.bounds.extents.z;
+        var planeColliderBounds = planeCollider.bounds;
+
+        var effectiveHorizontalOffset = horizontalOffset + playerHalfWidth;
+        var effectiveVerticalOffset = verticalOffset + playerHalfHeight;
+
+        _minHorizontalMovable = planeColliderBounds.min.x + effectiveHorizontalOffset;
+        _maxHorizontalMovable = planeColliderBounds.max.x - effectiveHorizontalOffset;
+        _minVerticalMovable = planeColliderBounds.min.z + effectiveVerticalOffset;
+        _maxVerticalMovable = planeColliderBounds.max.z - effectiveVerticalOffset;
     }
 
     private void Update()
@@ -34,27 +53,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetInputVector()
     {
-        _input = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        _input = new Vector3(joystick.Horizontal, 0, joystick.Vertical).normalized;
     }
 
     private void MovePosition()
     {
-        var playerHalfWidth = _playerCollider.bounds.extents.x;
-        var playerHalfHeight = _playerCollider.bounds.extents.z;
+        var targetVelocity = new Vector3(
+            _input.x * horizontalMoveSpeed,
+            0,
+            _input.z * verticalMoveSpeed
+        );
 
-        var effectiveHorizontalOffset = horizontalOffset + playerHalfWidth;
-        var effectiveVerticalOffset = verticalOffset + playerHalfHeight;
+        _currentVelocity = Vector3.MoveTowards(
+            _currentVelocity,
+            targetVelocity,
+            (_input.magnitude > 0 ? acceleration : deceleration) * Time.fixedDeltaTime
+        );
 
-        var desiredPosition = transform.position +
-                              transform.TransformDirection(new Vector3(
-                                  _input.x * horizontalMoveSpeed * Time.fixedDeltaTime,
-                                  0,
-                                  _input.z * verticalMoveSpeed * Time.fixedDeltaTime
-                              ));
+        var desiredPosition = transform.position + transform.TransformDirection(_currentVelocity * Time.fixedDeltaTime);
 
-        var bounds = _planeCollider.bounds;
-        var clampedX = Mathf.Clamp(desiredPosition.x, bounds.min.x + effectiveHorizontalOffset, bounds.max.x - effectiveHorizontalOffset);
-        var clampedZ = Mathf.Clamp(desiredPosition.z, bounds.min.z + effectiveVerticalOffset, bounds.max.z - effectiveVerticalOffset);
+        var clampedX = Mathf.Clamp(desiredPosition.x, _minHorizontalMovable, _maxHorizontalMovable);
+        var clampedZ = Mathf.Clamp(desiredPosition.z, _minVerticalMovable, _maxVerticalMovable);
 
         _rb.MovePosition(new Vector3(clampedX, transform.position.y, clampedZ));
     }
